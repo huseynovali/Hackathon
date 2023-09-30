d7fd629ea98a74bba0cccb3e6bf1c955538edcef
User <huseynovali612@gmail.com>
2023-09-30 13:37:16 +0400

    change

--- client/src/pages/adminPage/AdminPage.jsx
+++ client/src/pages/adminPage/AdminPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import Button from '@mui/material/Button';
 function AdminPage() {
   return (
-    <div>AdminPage</div>
+    <div><Button variant="contained">Hello world</Button></div>
   )
 }
 
