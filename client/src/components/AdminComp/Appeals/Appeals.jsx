af03a811074b3fa1c0c5266f0747422e301a6e13
User <huseynovali612@gmail.com>
2023-09-30 15:53:13 +0400

    change

---
+++ client/src/components/AdminComp/Appeals/Appeals.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Appeals.css'
+function Appeals() {
+  return (
+    <div className='appeals__container'>
+     <h1 className='appeals__header__word'>Appeals</h1>
+
+
+    </div>
+  )
+}
+
+export default Appeals
--- client/src/pages/adminPage/AdminPage.jsx
+++ client/src/pages/adminPage/AdminPage.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import Button from '@mui/material/Button';
-import Index from '../../components/AdminComp/Index';
+import React from "react";
+import AdminSideBar from "../../components/AdminComp/AdminSidebar/AdminSideBar"
+import Header from "../../components/AdminComp/Header/Header";
+import { Outlet } from "react-router";
 function AdminPage() {
   return (
-    <div>
-   <Index/>
-
-      
+    <div className="d-flex">
+       <AdminSideBar/>
+       <div className="d-flex flex-col">
+        <Header/>
+        <Outlet/>
+       </div>
     </div>
-  )
+  );
 }
 
-export default AdminPage
+export default AdminPage;
