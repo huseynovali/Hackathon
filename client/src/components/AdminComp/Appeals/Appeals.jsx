c4263d6dad52f6723433a3b554e868fbdb34b99c
RavanaPiriyeva <7j59c43@code.edu.az>
2023-09-30 17:19:03 +0400

    detail

--- client/src/components/AdminComp/Appeals/Appeals.jsx
+++ client/src/components/AdminComp/Appeals/Appeals.jsx
@@ -1,12 +1,24 @@
+import { Button } from '@mui/material'
 import React from 'react'
+import ShowDetail from '../ShowDetail/ShowDetail'
 import './Appeals.css'
+import { useDispatch, useSelector } from "react-redux";
+import { setShowTrue} from "../../../Store/detailSlice";
 function Appeals() {
+  const dispatch = useDispatch();
+  const { show } = useSelector(state => state.detailSlice);
+  console.log(show)
+
+  const handleOpen = ()=>{
+    dispatch(setShowTrue())
+ }
   return (
     <div className='appeals__container'>
-     <h1 className='appeals__header__word'>Appeals</h1>
+      <h1 className='appeals__header__word'>Appeals</h1>
 
-
-    </div>
+      <Button onClick={()=>handleOpen()}>show</Button>
+      {show ? <ShowDetail /> : <></>}
+    </div >
   )
 }
 
